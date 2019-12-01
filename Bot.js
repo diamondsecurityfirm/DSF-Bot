1dc2a2056954455cb19252f22caa73879d8985ab
diamondsecurityfirm <41444551+diamondsecurityfirm@users.noreply.github.com>
2019-11-30 23:53:26 -0600

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -148,4 +148,5 @@ client.on('message', message => {
                         }
                     }
                 })
-            }); client.login(process.env.BOT_TOKEN);
+            };
+    client.login(process.env.BOT_TOKEN);
